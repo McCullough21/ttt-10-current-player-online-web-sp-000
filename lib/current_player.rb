b6caf3b89f72c6ad43a3e79f6644e10b512e0def
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 13:24:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def turn_count(board)
   count = 0
   board.each do |space|
@@ -6,4 +7,12 @@ def turn_count(board)
     end
   end
   count
+=======
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+  count = 0
+  board.each do |board|
+   puts "#{board}"
+ end
+>>>>>>> 669417e925821add8299f3a740741d8fa72137b4
 end
